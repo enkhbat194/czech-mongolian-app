7a9d2c0fe6ee584ef28dedde80e36d02e004822d
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 04:25:59 +0800

    feat(a0.2): use manual reward and phrase-memory lesson engine

--- src/pages/A0NeedsPage.tsx
+++ src/pages/A0NeedsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import A0LessonEngine from '../components/lessons/A0LessonEngineV4';
+import A0LessonEngine from '../components/lessons/A0LessonEngineV5';
 import A0CarryoverReview from '../components/lessons/A0CarryoverReview';
 import { a0NeedsCards, a0NeedsMicroLessons, getA0NeedsCard } from '../data/a0Needs';
 import { a0NeedsCoveredFinalDialogue, a0NeedsCoveredMicroDialogues } from '../data/a0CoverageDialogues';
