4cfdb3c3d2e688102415bb5d91726bc4da210658
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 02:21:17 +0800

    feat(a0.2): use carryover review and coverage-reinforced dialogues

--- src/pages/A0NeedsPage.tsx
+++ src/pages/A0NeedsPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import A0LessonEngine from '../components/lessons/A0LessonEngineV4';
 import A0CarryoverReview from '../components/lessons/A0CarryoverReview';
 import { a0NeedsCards, a0NeedsMicroLessons, getA0NeedsCard } from '../data/a0Needs';
-import { a0NeedsFinalDialogue, a0NeedsMicroDialogues } from '../data/a0Dialogues';
+import { a0NeedsCoveredFinalDialogue, a0NeedsCoveredMicroDialogues } from '../data/a0CoverageDialogues';
 
 const A0NeedsPage: React.FC = () => {
   const [carryoverComplete, setCarryoverComplete] = useState(false);
@@ -20,8 +20,8 @@ const A0NeedsPage: React.FC = () => {
         cards: a0NeedsCards,
         microLessons: a0NeedsMicroLessons,
         getCard: getA0NeedsCard,
-        microDialogues: a0NeedsMicroDialogues,
-        finalDialogue: a0NeedsFinalDialogue,
+        microDialogues: a0NeedsCoveredMicroDialogues,
+        finalDialogue: a0NeedsCoveredFinalDialogue,
         xpReward: 160,
         completionIcon: '🧾',
         completionSummaryMn: 'Та одоо тусламж, ус, утас хэрэгтэйгээ хэлж, хүсэлтээ илэрхийлж, мөнгө эсвэл карт байхгүйгээ тайлбарлаж чадна.',
