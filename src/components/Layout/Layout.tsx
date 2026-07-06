513dfe9606cebf0604ade068a4ad7844309b50b1
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 17:00:52 +0800

    refactor(a0): route all ready A0 lessons through generic page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,9 +16,7 @@ import WritingPage from '../../pages/WritingPage';
 import DictationPage from '../../pages/DictationPage';
 import FillBlankPage from '../../pages/FillBlankPage';
 import InteractiveLearningPage from '../../pages/InteractiveLearningPage';
-import A0FirstContactPage from '../../pages/A0FirstContactPage';
-import A0NeedsPage from '../../pages/A0NeedsPage';
-import A0LocationPage from '../../pages/A0LocationPage';
+import A0ReferenceLessonPage from '../../pages/A0ReferenceLessonPage';
 import TodayReviewPage from '../../pages/TodayReviewPage';
 
 const v: Variants = {
@@ -42,16 +40,14 @@ const pages: Record<string, React.FC> = {
   dictation: DictationPage,
   fillBlank: FillBlankPage,
   interactiveLearning: InteractiveLearningPage,
-  a0FirstContact: A0FirstContactPage,
-  a0Needs: A0NeedsPage,
-  a0Location: A0LocationPage,
+  a0Lesson: A0ReferenceLessonPage,
   todayReview: TodayReviewPage,
 };
 
 const Layout: React.FC = () => {
   const { currentPage } = useAppStore();
   const Page = pages[currentPage] || HomePage;
-  const isImmersiveLesson = ['a0FirstContact', 'a0Needs', 'a0Location', 'todayReview'].includes(currentPage);
+  const isImmersiveLesson = ['a0Lesson', 'todayReview'].includes(currentPage);
 
   return (
     <div style={{ minHeight: '100dvh', background: '#080810', display: 'flex', justifyContent: 'center' }}>
