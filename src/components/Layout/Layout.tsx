98fa3d4ef2596213c560777823e5ecab82b18b6e
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-07 07:07:23 +0800

    refactor: consolidate learner A0 routes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -17,7 +17,6 @@ import DictationPage from '../../pages/DictationPage';
 import FillBlankPage from '../../pages/FillBlankPage';
 import InteractiveLearningPage from '../../pages/InteractiveLearningPage';
 import A0ReferenceLessonPage from '../../pages/A0ReferenceLessonPage';
-import A0DialoguePreviewPage from '../../pages/A0DialoguePreviewPage';
 import TodayReviewPage from '../../pages/TodayReviewPage';
 
 const v: Variants = {
@@ -41,18 +40,14 @@ const pages: Record<string, React.FC> = {
   dictation: DictationPage,
   fillBlank: FillBlankPage,
   interactiveLearning: InteractiveLearningPage,
-  a0DialoguePreview: A0DialoguePreviewPage,
   todayReview: TodayReviewPage,
 };
 
-const genericA0Pages = new Set(['a0FirstContact', 'a0Needs', 'a0Location', 'a0Lesson']);
-const genericLessonIds = new Set(['l004', 'l005', 'l006']);
-
 const Layout: React.FC = () => {
   const { currentPage, currentLessonId } = useAppStore();
-  const isGenericA0 = genericA0Pages.has(currentPage) || (currentPage === 'flashcard' && genericLessonIds.has(currentLessonId || ''));
-  const Page = isGenericA0 ? A0ReferenceLessonPage : (pages[currentPage] || HomePage);
-  const isImmersiveLesson = isGenericA0 || currentPage === 'todayReview' || currentPage === 'a0DialoguePreview';
+  const isA0Lesson = currentPage === 'a0Lesson';
+  const Page = isA0Lesson ? A0ReferenceLessonPage : (pages[currentPage] || HomePage);
+  const isImmersiveLesson = isA0Lesson || currentPage === 'todayReview';
 
   return (
     <div style={{ minHeight: '100dvh', background: '#080810', display: 'flex', justifyContent: 'center' }}>
