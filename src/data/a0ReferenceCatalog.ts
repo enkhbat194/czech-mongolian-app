6ae4b733a03665c683d0ab49f1589e5366ac2914
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 17:25:57 +0800

    feat(a0): assemble generic lesson catalog through A0.6

---
+++ src/data/a0ReferenceCatalog.ts
@@ -0,0 +1,10 @@
+import './a0DirectionsMemory';
+import './a0TimeMemory';
+import './a0WorkMemory';
+import { a0ReferenceLessons } from './a0ReferenceLessons';
+import { a0ReferenceNewLessons } from './a0ReferenceNewLessons';
+
+export const a0ReferenceCatalog = {
+  ...a0ReferenceLessons,
+  ...a0ReferenceNewLessons,
+};
