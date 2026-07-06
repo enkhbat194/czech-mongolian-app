2d96f91f394f39c19ced2fcc31a9df4898240fee
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 19:24:04 +0800

    refactor(a0): make reference catalog use canonical registry

--- src/data/a0ReferenceCatalog.ts
+++ src/data/a0ReferenceCatalog.ts
@@ -1,7 +1,1 @@
-import { a0ReferenceLessons } from './a0ReferenceLessons';
-import { a0ReferenceNewLessons } from './a0ReferenceNewLessons';
-
-export const a0ReferenceCatalog = {
-  ...a0ReferenceLessons,
-  ...a0ReferenceNewLessons,
-};
+export { a0ReferenceLessons as a0ReferenceCatalog } from './a0ReferenceLessons';
