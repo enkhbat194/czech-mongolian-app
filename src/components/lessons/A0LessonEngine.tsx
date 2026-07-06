8607ccd8e3d9b9e17ae9c85f6b9ed9aa81962d21
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 11:49:20 +0800

    refactor(engine): expose schema-validated lesson engine interface

--- src/components/lessons/A0LessonEngine.tsx
+++ src/components/lessons/A0LessonEngine.tsx
@@ -1,2 +1,11 @@
-export { default } from './A0LessonEngineV5';
-export type { A0LessonEngineConfig } from './A0LessonEngineV5';
+import React from 'react';
+import LegacyA0LessonEngine from './A0LessonEngineV5';
+import type { A0LessonEngineConfig as LegacyA0LessonEngineConfig } from './A0LessonEngineV5';
+import type { A0LessonDefinition } from '../../data/a0LessonSchema';
+
+const A0LessonEngine: React.FC<{ config: A0LessonDefinition }> = ({ config }) => (
+  <LegacyA0LessonEngine config={config as unknown as LegacyA0LessonEngineConfig} />
+);
+
+export default A0LessonEngine;
+export type { A0LessonDefinition } from '../../data/a0LessonSchema';
