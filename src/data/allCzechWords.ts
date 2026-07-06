41cb6c00039283041d62f3a716973c01d939a50d
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-06 17:13:07 +0800

    feat(a0): aggregate card data through A0.6

---
+++ src/data/allCzechWords.ts
@@ -0,0 +1,11 @@
+import { czechWords as foundationWords } from './czechWords';
+import { a0DirectionsWords } from './a0DirectionsWords';
+import { a0TimeWords } from './a0TimeWords';
+import { a0JobWords } from './a0JobWords';
+
+export const allCzechWords = [
+  ...foundationWords,
+  ...a0DirectionsWords,
+  ...a0TimeWords,
+  ...a0JobWords,
+];
