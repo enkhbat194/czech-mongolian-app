c1a294d691c2dd621101e267db0eba81928524ea
enkhbat194 <34760209+enkhbat194@users.noreply.github.com>
2026-07-17 09:08:12 +0800

    feat: expose corrected Czech word catalog

--- src/data/allCzechWords.ts
+++ src/data/allCzechWords.ts
@@ -1,3 +1,4 @@
+import { repairA0WordLanguage } from './a0LanguageCorrections';
 import { czechWords } from './czechWords';
 
-export const allCzechWords = czechWords;
+export const allCzechWords = czechWords.map((word) => repairA0WordLanguage(word));
